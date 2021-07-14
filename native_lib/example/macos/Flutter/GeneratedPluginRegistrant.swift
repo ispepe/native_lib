912559af6c1692aa68471ec9805e3a476b4734f9
Pepe <jp@jin.dev>
2021-07-14 15:36:03 +0800

    init projedt

---
+++ native_lib/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import native_lib
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NativeLibPlugin.register(with: registry.registrar(forPlugin: "NativeLibPlugin"))
+}
